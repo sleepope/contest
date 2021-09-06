1d98ed2f20c8942d9e78e6806c37fcc26e445458
sleepope <SciZeal@outlook.com>
2021-09-06 17:31:48 +0800

    update sample i/o by A+B problem

--- check/randgen.py
+++ check/randgen.py
@@ -1,10 +1,10 @@
 import sys
 from random import *
 
-# seed(12345)
+seed(12345)
 sys.stdout = open(r'../hack.txt', 'w')
 
-n = int(1e3)
+n = int(1e2)
 print(n)
 for i in range(n):
     a = randint(-1e9, 1e9)
